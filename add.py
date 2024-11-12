cb6e22cbdb1f19e35aaffc9334b0d46a4620aad4
xavier quishpe <quishpexavier27@gmail.com>
2024-11-12 10:46:12 -0500

    feat: :memo: Update the code
    Update name add.py

--- add.py
+++ add.py
@@ -1,9 +1,10 @@
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hola Mundo!"
 
-if __name__ == '__add.py__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
     
